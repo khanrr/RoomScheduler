24eb22de220f67f97dd8a98aa313ca7d4884b7c4
khanrr <47981078+khanrr@users.noreply.github.com>
2019-12-12 17:38:20 -0500

    Update WaitlistQueries.java
    Updated code for WaitlistQueries.

--- Java/Room-Scheduler/WaitlistQueries.java
+++ Java/Room-Scheduler/WaitlistQueries.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Calendar;
 /**
  *
@@ -13,11 +14,19 @@ public class WaitlistQueries {
     private static Connection connection;
     private static PreparedStatement addWaitlist;
     private static ResultSet resultSetWaitlist;
+    private static PreparedStatement getWaitlist;
+    private static ResultSet resultSet;
     
-    public static void addWaitlist(String name, String date, Integer seats){
+    public static void addWaitlist(Object faculty, Object date, int seats){
     connection = DBConnection.getConnection();
     try{
-        addWaitlist = connection.prepareStatement("insert into waitlist (faculty, date, seats, timestamp) values ('"+name+"', '"+date+"', '"+seats+"', current_timestamp)");
+        java.sql.Timestamp currentTimestamp = new java.sql.Timestamp(Calendar.getInstance().getTime().getTime());
+        
+        addWaitlist = connection.prepareStatement("insert into waitlist (faculty, date, seats, timestamp) values (?,?,?,?)");
+        addWaitlist.setString(1, faculty.toString());
+        addWaitlist.setString(2, date.toString());
+        addWaitlist.setInt(3, seats);
+        addWaitlist.setTimestamp(4, currentTimestamp);
         addWaitlist.executeUpdate();
     }
     catch(SQLException sqlException){
@@ -25,9 +34,22 @@ public class WaitlistQueries {
     }
     }
     
-    public static String getWaitlist(){
-        String x = null;
-        return x; 
+    public static ArrayList<String> getWaitlist(){
+        connection = DBConnection.getConnection();
+        ArrayList<String> waitlist = new ArrayList<String>();
+        try{
+            getWaitlist = connection.prepareStatement("select * from waitlist order by timestamp");
+            resultSet = getWaitlist.executeQuery();
+            
+            while(resultSet.next()){
+                waitlist.add(resultSet.getString(1)  + " " + resultSet.getString(2) + " " + resultSet.getString(3));
+                
+            }
+        }
+        catch(SQLException sqlException){
+            sqlException.printStackTrace();
+        }
+        return waitlist;
+         
     } 
 }
-    
