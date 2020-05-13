87fa57feeefb73cd5f91bcd2d668138dca543000
khanrr <47981078+khanrr@users.noreply.github.com>
2020-05-13 18:44:19 -0400

    Upload Password Manager project

---
+++ PasswordManager.java
@@ -0,0 +1,16 @@
+package passwordmanager;
+
+/**
+ *
+ * @author Rian Khan
+ */
+public class PasswordManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PMCtrl pmCtrl = new PMCtrl();
+    }
+    
+}
